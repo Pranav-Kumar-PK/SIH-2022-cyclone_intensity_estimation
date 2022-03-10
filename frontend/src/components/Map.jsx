ebd9b7b04ee4d611b4438f2b625d3eee725d90ba
Pranav-Kumar-PK <https://github.com/Pranav-Kumar-PK>
2022-03-10 21:21:44 +0530

    map added

---
+++ frontend/src/components/Map.jsx
@@ -0,0 +1,44 @@
+import React, { useRef, useEffect, useState } from 'react';
+import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+import SideBarWrap from './sidewrap';
+import Navbar from './navbar';
+
+mapboxgl.accessToken = 'pk.eyJ1IjoicHJhbmF2MTI5OCIsImEiOiJja3NjMWxjOTMwYzRkMm9xcTUxNXFpYzl5In0._gL-06fXtg1yBszkiiFjEQ';
+//this is a new branch
+export default function Map() {
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const [lng, setLng] = useState(82.5699);
+  const [lat, setLat] = useState(22.1957);
+  const [zoom, setZoom] = useState(3.35);
+
+  useEffect(() => {
+    if (map.current) return; // initialize map only once
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: 'mapbox://styles/mapbox/satellite-streets-v11',
+      center: [lng, lat],
+      zoom: zoom
+    });
+  });
+
+  useEffect(() => {
+    if (!map.current) return; // wait for map to initialize
+    map.current.on('move', () => {
+      setLng(map.current.getCenter().lng.toFixed(4));
+      setLat(map.current.getCenter().lat.toFixed(4));
+      setZoom(map.current.getZoom().toFixed(2));
+    });
+  });
+
+  return (
+    <div>
+      <div className="map">
+        Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+      </div>
+      <div ref={mapContainer} className="map-container" />
+      <Navbar/>
+      <SideBarWrap/>
+    </div>
+  );
+}
