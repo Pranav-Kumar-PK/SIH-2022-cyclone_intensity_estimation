f849117865f6e5a82be05cee6c610df204775249
Vanshika  Agarwal <agarwalvanshika351@gmail.com>
2022-03-13 20:18:45 +0530

    input changes

--- frontend/src/components/slider.jsx
+++ frontend/src/components/slider.jsx
@@ -11,7 +11,7 @@ const Slider = () => {
             src="https://images.squarespace-cdn.com/content/v1/55b29de4e4b088f33db802c6/1478191160620-JWRAE5OEJACLBG8E5ZFX/PlasticBagsLandfill.jpg?format=1500w"
             alt="First slide"
           />
-          <input type="text" placeholder="Category" />
+          <input className="input" type="text" placeholder="Category" />
         </Carousel.Item>
         <Carousel.Item>
           <img
@@ -19,7 +19,7 @@ const Slider = () => {
             src="https://thumbs.dreamstime.com/b/garbage-dump-site-outside-town-129978999.jpg"
             alt="Second slide"
           />
-          <input type="text" placeholder="Category" />
+          <input type="text" className="input" placeholder="Category" />
         </Carousel.Item>
 
         <Carousel.Item>
@@ -28,7 +28,7 @@ const Slider = () => {
             src="https://media.istockphoto.com/photos/garbage-pile-in-trash-dump-or-landfill-pollution-concept-picture-id845816364?k=20&m=845816364&s=612x612&w=0&h=tpbBMmjQG3mtIvMJVJY03GP_IFt4cpGVrBcplJ5qo0A="
             alt="Third slide"
           />
-          <input type="text" placeholder="Category" />
+          <input type="text"  className="input"placeholder="Category" />
           <button>Submit</button>
         </Carousel.Item>
       </Carousel>
