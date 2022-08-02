5e24afd4fe455328190ee28256ac2e48e8d35cd9
Vanshika  Agarwal <agarwalvanshika351@gmail.com>
2022-08-02 13:36:31 +0530

    Sidebar changes

--- frontend/src/components/sidewrap.jsx
+++ frontend/src/components/sidewrap.jsx
@@ -1,17 +1,30 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-//import { SidebarData } from './sidebarData';
+// import { SidebarData } from './sidebarData';
 
 const SideBarWrap = (props) => {
   return (
     <div id="sidebar" className="sidebar-wrapper d-none d-md-block">
       <div className="sidebar-content">
-        {/* <a className="sidebar-logo" href="../">
+        <a className="sidebar-logo" href="../">
           <span>DISCOVER</span>
-        </a> */}
+        </a>
       </div>
-      <nav id="filter-group" className="filter-group"></nav>
+      {/* <nav id="filter-group" className="filter-group"></nav> */}
+      <ul>
+                    <li>                        <Link to="/clients" className=""><i className="fas fa-user-tie gradient__btn gradient__btn-10"></i> <span>Clients</span></Link>
+                    </li>
+                    <li>
+                        <Link to="/project"><i className="fas fa-project-diagram"></i> <span>Project</span></Link>
+                    </li>
+                    <li>
+                        <Link to="/member"><i className="fas fa-users"></i> <span>Members</span></Link>
+                    </li>
+                    <li>                       <Link to="/tasks"><i className="fas fa-tasks"></i> <span>Tasks</span></Link>
+                    </li>
+                </ul>
     </div>
+     
   );
 };
 
